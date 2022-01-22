8f45ab8ad8f488cc5acd007ce10bbd8aa03bf3b6
Antonio Cavallo <a.cavallo@cavallinux.eu>
2022-01-21 21:50:01 -0500

    initial

--- tests/test_doc2lit.py
+++ tests/test_doc2lit.py
@@ -10,15 +10,20 @@ with more line
 if __name__ == "__main__":
     pass
 """
-    assert doc2lit.get_doc(txt) == """
+    assert (
+        doc2lit.get_doc(txt)
+        == """
 A doc example
 with more line
 """.strip()
+    )
 
 
 def test_load_doc(datadir):
     txt = doc2lit.load_doc(datadir / "testdoc.py")
-    assert txt == """
+    assert (
+        txt
+        == """
 a simple test doc
 
 This is a simple test doc to use
@@ -32,12 +37,15 @@ multi: a multiline
       on multiple lines
 ---
 """.strip()
+    )
 
 
 def test_process_markdown(datadir):
     txt = doc2lit.load_doc(datadir / "testdoc.py")
     html, meta = doc2lit.process_markdown(txt)
     assert html == "<p>This is a simple test doc to use\n  for testing</p>\n"
-    assert meta == {'author': 'an author name',
-                    'multi': 'a multiline\n    text with indentation\n      on multiple lines',
-                    'title': 'a title'}
+    assert meta == {
+        "author": "an author name",
+        "multi": "a multiline\n    text with indentation\n      on multiple lines",
+        "title": "a title",
+    }
